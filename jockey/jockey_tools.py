9aeadf2b32c36cb8e0c4439eea452e04e5feedbe
Travis Couture <travis@twelvelabs.io>
2024-04-25 09:26:13 -0600

    Changed video downloads from tool to internal function call.

--- jockey/jockey_tools.py
+++ jockey/jockey_tools.py
@@ -5,7 +5,7 @@ import ffmpeg
 from langchain.tools import tool
 from langchain.pydantic_v1 import BaseModel, Field
 from typing import List, Dict, Union
-from util import get_video_metadata
+from util import get_video_metadata, download_video
 
 
 TL_BASE_URL = "https://api.twelvelabs.io/v1.2/"
@@ -16,19 +16,26 @@ GIST_URL = urllib.parse.urljoin(TL_BASE_URL, "gist/")
 SUMMARIZE_URL = urllib.parse.urljoin(TL_BASE_URL, "summarize/")
 GENERATE_URL = urllib.parse.urljoin(TL_BASE_URL, "generate/")
 
-PEGASUS_INDEX_ID = "659ecd3e6bb8e7df9af19eaa"
-MARENGO_INDEX_ID = "659f2e829aba4f0b402f6488"
-
 class MarengoSearchInput(BaseModel):
-    query: str = Field(description="Search query to run on a collection of videos.")
+    query: str | dict = Field(description="Search query to run on a collection of videos.")
     index_id: str = Field(description="Index ID which contains a collection of videos.")
     top_n: int = Field(description="Used to select the top N results of a search.", gt=0, le=10)
     group_by: str = Field(description="Used to decide how to group search results. Must be one of: `clip` or `video`.")
+    video_filter: List[str] = Field(description="Filter search results to only include results from video IDs in this list.")
 
 
 @tool("video-search", args_schema=MarengoSearchInput)
-async def video_search(query: str, index_id: str, top_n: int = 3, group_by: str = "clip") -> Union[List[Dict], List]:
-    """Run a search query against a collection of videos and get results."""
+def video_search(query: str | dict, index_id: str, top_n: int = 3, group_by: str = "clip", video_filter: list = None) -> Union[List[Dict], List]:
+    """Run a search query against a collection of videos and get results. Search queries can be or complex.
+    Simple Query Example: {"text": "a dog playing with a yellow and white tennis ball"}
+
+    Complex queries use logical operators to wrap simple queries. Here are the logical operators you can use:
+        $and -- every simple search query must be true for a result to appear
+            {$and: [<simple_query_1>, <simple_query_2>, ...]}
+        $or -- only a single simple query must be true for a result to appear
+            {$or: [<simple_query_1>, <simple_query_2>, ...]}
+        $not: -- consists of "origin" and "sub" keys where origin is the inclusive set of queries and sub is exclusionary 
+            {$not: {"origin": {<simple_query>} | [<logical_operator_queries>], "sub": {<simple_query>} | [<logical_operator_queries>]}"""
     headers = {
         "x-api-key": os.environ["TWELVE_LABS_API_KEY"],
         "accept": "application/json",
@@ -36,16 +43,18 @@ async def video_search(query: str, index_id: str, top_n: int = 3, group_by: str 
     }
 
     payload = {
-        "search_options": ["visual", "conversation"],
+        "search_options": ["visual"],
         "group_by": group_by,
         "threshold": "low",
         "sort_option": "score",
-        "operator": "or",
         "conversation_option": "semantic",
         "page_limit": top_n,
         "index_id": index_id,
         "query": query
     }
+
+    if filter is not None:
+        payload["filter"] = {"id": video_filter}
 
     response = requests.post(SEARCH_URL, json=payload, headers=headers)
 
@@ -85,44 +94,6 @@ async def video_search(query: str, index_id: str, top_n: int = 3, group_by: str 
     return top_n_results
 
 
-class DownloadVideoInput(BaseModel):
-    video_id: str = Field(description="Video ID used to download a video. It is also used as the filename for the video.")
-    index_id: str = Field(description="Index ID which contains a collection of videos.")
-
-
-@tool("video-download", args_schema=DownloadVideoInput)
-def download_video(video_id: str, index_id: str) -> str:
-    """Download a video for a given video in a given index and get the filepath. 
-    Should only be used when the user explicitly requests video editing functionalities."""
-    headers = {
-        "x-api-key": os.environ["TWELVE_LABS_API_KEY"],
-        "accept": "application/json",
-        "Content-Type": "application/json"
-    }
-
-    video_url = f"https://api.twelvelabs.io/v1.2/indexes/{MARENGO_INDEX_ID}/videos/{video_id}"
-
-    response = requests.get(video_url, headers=headers)
-
-    hls_uri = response.json()["hls"]["video_url"]
-
-    video_dir = os.path.join(os.getcwd(), index_id)
-    video_filename = f"{video_id}.mp4"
-    video_path = os.path.join(video_dir, video_filename)
-
-    if os.path.isfile(video_path) is False:
-        try:
-            ffmpeg.input(filename=hls_uri, strict="experimental", loglevel="quiet").output(video_path, vcodec="copy", acodec="libmp3lame").run()
-        except Exception as error:
-            error_response = {
-                "message": "There was a video editing error.",
-                "error": error
-            }
-            return error_response
-
-    return video_path
-
-
 class CombineClipsInput(BaseModel):
     clips: List = Field(description="""Clip results found using the video-search tool.""")
     queries: List[str] = Field(description="The search queries passed to the video-search tool to find the clips. One for each clip.")
@@ -131,7 +102,7 @@ class CombineClipsInput(BaseModel):
 
 
 @tool("combine-clips", args_schema=CombineClipsInput)
-def combine_clips(clips: List[Dict], queries: List[str], output_filename: str, index_id: str) -> str:
+def combine_clips(clips: List[Dict], queries: List[str] | List[dict], output_filename: str, index_id: str) -> str:
     """Combine or edit multiple clips together based on video IDs that are results from the video-search tool. The full filepath for the combined clips is returned."""
     try:
         input_streams = []
@@ -139,12 +110,15 @@ def combine_clips(clips: List[Dict], queries: List[str], output_filename: str, i
 
         for clip, query in zip(clips, queries):
             video_id = clip["video_id"]
-            video_filepath = os.path.join(os.getcwd(), index_id, f"{video_id}.mp4")
             start = clip["start"]
             end = clip["end"]
+            video_filepath = os.path.join(os.getcwd(), index_id, f"{video_id}_{start}_{end}.mp4")
 
-            video_input_stream = ffmpeg.input(filename=video_filepath, loglevel="quiet").video.filter("trim", start=start, end=end).filter("setpts", "PTS-STARTPTS")
-            audio_input_stream = ffmpeg.input(filename=video_filepath, loglevel="quiet").audio.filter("atrim", start=start, end=end).filter("asetpts", "PTS-STARTPTS")
+            if os.path.isfile(video_filepath) is False:
+                download_video(video_id=video_id, index_id=index_id, start=start, end=end)
+
+            video_input_stream = ffmpeg.input(filename=video_filepath, loglevel="quiet").video
+            audio_input_stream = ffmpeg.input(filename=video_filepath, loglevel="quiet").audio
             clip_with_text_stream = video_input_stream.drawtext(text=query, x="(w-text_w)/2", fontfile=arial_font_file, box=1, 
                                                                 boxcolor="black", fontcolor="white", fontsize=28)
             
@@ -188,18 +162,19 @@ def remove_segment(video_filepath: str, start: float, end: float) -> str:
 
 class PegasusGenerateInput(BaseModel):
     video_id: str = Field(description="The ID of the video to generate text from.")
-    endpoint: str = Field(description="""What type of text to generate from a video: must be one of: ['gist', 'summarize', 'generate']
-                                      gist: For topics, titles, and hashtags using predefined formats.
-                                      summarize: For summaries, chapters, and highlights, allowing customization with a prompt
-                                      generate: For open-ended text, requiring clear instructions in the form of a prompt to guide the output.""")
-    prompt: str = Field(default=None, description="The prompt to be used when generating output for a certain task. Only required for the 'generate' endpoint and optional for the 'summarize' endpoint.")
-    endpoint_options: Union[str, List[str]] = Field(default=None, description="""Determines what the user wishes to generate. For the 'summarize' endpoint must be exactly one of: ['summary', 'highlight', 'chapter'].
-                                                                              For the 'gist' endpoint can be any combination of: ['topic', 'hashtag', 'title'].""")
+    endpoint: str = Field(description="""What type of text to generate from a video: must be one of: ['gist', 'summarize', 'generate']""")
+    prompt: str = Field(default=None, description="""The prompt to be used by the video foundation model to generate text output.
+                                                  Can be used with the `summarize` or `generate` endpoints. Must be 300 characters or less.""")
+    endpoint_options: Union[str, List[str]] = Field(default=None, 
+                                                    description="""Determines what the user wishes to generate. Required for `summarize` and `gist` endpoints.
+                                                                For the 'summarize' endpoint must be exactly one of: ['summary', 'highlight', 'chapter'].
+                                                                For the 'gist' endpoint can be any combination of: ['topic', 'hashtag', 'title'].
+                                                                There are no `endpoint_options` for the `generate` endpoint.""")
 
 
 @tool("video-text-generation", args_schema=PegasusGenerateInput)
-def video_text_generation(video_id: str, endpoint: str, prompt: str = None, endpoint_options: Union[str, List[str]] = None):
-    """Generate text output for a single video. The text generated can have various options depending on the user's request."""
+async def video_text_generation(video_id: str, endpoint: str, prompt: str = None, endpoint_options: Union[str, List[str]] = None):
+    """Generate text output for a single video based on what the user wants to know."""
 
     headers = {
             "accept": "application/json",
@@ -223,7 +198,7 @@ def video_text_generation(video_id: str, endpoint: str, prompt: str = None, endp
 
         if endpoint_options is not None:
             payload["type"] = endpoint_options
-    elif endpoint == "generate":
+    if endpoint == "generate":
         url = GENERATE_URL
 
     if prompt is not None:
@@ -233,4 +208,4 @@ def video_text_generation(video_id: str, endpoint: str, prompt: str = None, endp
 
     return response.json()
 
-JOCKEY_TOOLKIT = [video_search, download_video, combine_clips, remove_segment, video_text_generation]
+JOCKEY_TOOLKIT = [video_search, combine_clips, remove_segment, video_text_generation]
