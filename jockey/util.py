b8bf06fec3778ac4debf32477e35541d3182ffcf
Travis Couture <travis@twelvelabs.io>
2024-04-25 09:26:44 -0600

    Made download_video a util.

--- jockey/util.py
+++ jockey/util.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import urllib
+import ffmpeg
 from langchain_core.callbacks.base import AsyncCallbackHandler
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 from uuid import UUID
@@ -61,3 +62,40 @@ def get_video_metadata(index_id: str, video_id: str) -> dict:
     response = requests.get(video_url, headers=headers)
 
     return response
+
+
+def download_video(video_id: str, index_id: str, start: float, end: float) -> str:
+    """Download a video for a given video in a given index and get the filepath. 
+    Should only be used when the user explicitly requests video editing functionalities."""
+    headers = {
+        "x-api-key": os.environ["TWELVE_LABS_API_KEY"],
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    video_url = f"https://api.twelvelabs.io/v1.2/indexes/{index_id}/videos/{video_id}"
+
+    response = requests.get(video_url, headers=headers)
+
+    hls_uri = response.json()["hls"]["video_url"]
+
+    video_dir = os.path.join(os.getcwd(), index_id)
+
+    if os.path.isdir(video_dir) is False:
+        os.mkdir(video_dir)
+
+    video_filename = f"{video_id}_{start}_{end}.mp4"
+    video_path = os.path.join(video_dir, video_filename)
+
+    if os.path.isfile(video_path) is False:
+        try:
+            duration = end - start
+            ffmpeg.input(filename=hls_uri, strict="experimental", loglevel="quiet", ss=start, t=duration).output(video_path, vcodec="copy", acodec="libmp3lame").run()
+        except Exception as error:
+            error_response = {
+                "message": "There was a video editing error.",
+                "error": error
+            }
+            return error_response
+
+    return video_path
