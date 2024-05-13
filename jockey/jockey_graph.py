05eee614c183ee0be70c15f6a68818aae61f3b4a
Travis Couture <travis@twelvelabs.io>
2024-05-12 21:47:37 -0600

    Changed graph construction and put a placeholder for a planner.

--- jockey/jockey_graph.py
+++ jockey/jockey_graph.py
@@ -1,4 +1,5 @@
 import functools
+import json
 from typing import Annotated, Type, Union, Sequence, Dict, TypedDict
 from langchain_openai.chat_models.base import BaseChatOpenAI
 from langchain_openai.chat_models.azure import AzureChatOpenAI
@@ -15,11 +16,12 @@ from langchain.tools import BaseTool, StructuredTool
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langgraph.checkpoint.aiosqlite import AsyncSqliteSaver
 from stirrups.video_search import build_video_search_worker
-from jockey_tools import combine_clips, remove_segment
+from stirrups.video_text_generation import build_video_text_generation_worker
+from stirrups.video_editing import build_video_editing_worker
 
 class JockeyState(TypedDict):
     chat_history: Annotated[Sequence[Union[BaseMessage | AIMessage | HumanMessage | SystemMessage]], add_messages]
-    next: str
+    next_worker: str
 
 
 class Jockey(StateGraph):
@@ -28,6 +30,8 @@ class Jockey(StateGraph):
     router: Dict
     base_prompt: str
     supervisor_llm: Union[BaseChatOpenAI | AzureChatOpenAI]
+    worker_llm: Union[BaseChatOpenAI | AzureChatOpenAI]
+    worker_instruction_generator: Runnable
 
     def __init__(self, supervisor_llm: Union[BaseChatOpenAI | AzureChatOpenAI], 
                        worker_llm: Union[BaseChatOpenAI | AzureChatOpenAI], 
@@ -36,10 +40,12 @@ class Jockey(StateGraph):
         super().__init__(JockeyState)
         self.base_prompt = prompt
         self.supervisor_llm = supervisor_llm
+        self.worker_llm = worker_llm
         core_workers = self.build_core_workers(worker_llm=worker_llm)
         self.workers = core_workers
         self.router = self.build_router()
         self.supervisor = self.build_supervisor()
+        self.worker_instruction_generator = self.build_worker_instruction_generator()
         self.construct_graph()
 
 
@@ -48,12 +54,14 @@ class Jockey(StateGraph):
             raise TypeError(f"Worker LLM must be one of: BaseChatOpenAI, AzureChatOpenAI. Got: {type(worker_llm).__name__}")
         
         video_search_worker = build_video_search_worker(worker_llm)
-        core_workers = [video_search_worker]
+        video_text_generation_worker = build_video_text_generation_worker(worker_llm)
+        video_editing_worker = build_video_editing_worker(worker_llm)
+        core_workers = [video_search_worker, video_text_generation_worker, video_editing_worker]
         return core_workers
         
 
     def build_router(self) -> Dict:
-        router_options = [worker.name for worker in self.workers] + ["REFLECT"]
+        router_options = [worker.name for worker in self.workers] + ["REFLECT", "supervisor", "planner"]
         router = {
             "name": "route",
             "description": "Determine whether to choose the next active worker or reflect.",
@@ -61,14 +69,14 @@ class Jockey(StateGraph):
                 "title": "routeSchema",
                 "type": "object",
                 "properties": {
-                    "next": {
-                        "title": "Next",
+                    "next_worker": {
+                        "title": "Next Worker",
                         "anyOf": [
                             {"enum": router_options},
                         ],
                     },
                 },
-                "required": ["next"],
+                "required": ["next_worker"],
             },
         }
         return router
@@ -77,32 +85,88 @@ class Jockey(StateGraph):
     def build_supervisor(self) -> Runnable:
         if any(map(lambda x: isinstance(self.supervisor_llm, x), [BaseChatOpenAI, AzureChatOpenAI])) is False:
             raise TypeError(f"Worker LLM must be one of: BaseChatOpenAI, AzureChatOpenAI. Got: {type(self.supervisor_llm).__name__}")
-        worker_names = str.join(", ", [worker.name for worker in self.workers])
+
         supervisor_prompt = ChatPromptTemplate.from_messages([
             ("system", self.base_prompt),
             MessagesPlaceholder(variable_name="chat_history"),
-            ("system", "Given the above conversation which worker should act next?"
-            "Alternatively you could choose to REFLECT to move on to reflect and provide a final response.")
-        ]).partial(workers=worker_names)
+            ("system", "Given the above conversation which worker should act next? "
+             "If you haven't called the planner worker for the current request(s) then heavily consider calling the planner worker. "
+             "Additional, if you are in the middle of executing a plan and something unexpected has happened, like an error, " 
+             "you can use the planner to create an updated plan and try again. "
+             "Alternatively you could choose to REFLECT to move on to reflect and provide a final response if all tasks have been completed.")
+        ])
+
         supervisor = supervisor_prompt | self.supervisor_llm.bind_functions(functions=[self.router], function_call="route") | JsonOutputFunctionsParser()
         return supervisor
     
 
-    async def worker_node(self, state: JockeyState, worker: AgentExecutor, name):
-        worker_response = await worker.ainvoke(state)
-        return {"chat_history": HumanMessage(content=str(worker_response), name=name)}
+    def build_worker_instruction_generator(self) -> Runnable:
+        instructions_prompt = ChatPromptTemplate.from_messages([
+            ("system", self.base_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("system", "The worker: {next_worker} has been selected to act next. "
+             "Given the above context, what simple natural language instructions should be passed to this worker? "
+             "Remember a worker can only execute a single task per request. " 
+             "Your response should be a single task for {next_worker} to complete.")
+        ])
+        worker_instruction_generator: Runnable = instructions_prompt | self.supervisor_llm
+        worker_instruction_generator = worker_instruction_generator.with_config({"tags": ["instructions_generator"]})
+        return worker_instruction_generator
     
 
-    async def reflect_node(self, state):
-        worker_names = str.join(", ", [worker.name for worker in self.workers])
+    async def planner_node(self, state: JockeyState) -> Runnable:
+        planner_prompt = ChatPromptTemplate.from_messages([
+            ("system", self.base_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("system", "You are a dedicated and competent planner for complex workflows and tasks especially those related to video editing. "
+             "Considering the context above, devise a detailed step-by-step plan that will complete all parts of the request. "
+             "As you plan you should consider how the output of one step will or could be used as input to later steps. "
+             "You should construct your overall plan in natural language and think aloud about how and why you constructed this plan. "
+             "Then construct a numbered list of steps to complete the plan you just created. "
+             "Each step should consist of a single task and the correct worker to complete that task. "
+             "The final step in your plan should always be reflection to ensure the plan went accordingly. "
+             "If you are replanning after encountering an error adjust your approach accordingly. "
+             "Limit your replanning efforts to 3 times or less if you keep encountering the same error.")
+        ])
+        planner_chain: Runnable = planner_prompt | self.supervisor_llm
+        planner_chain = planner_chain.with_config({"tags": ["planner"]})
+        planner_response = await planner_chain.ainvoke(state)
+        planner_response = HumanMessage(content=planner_response.content, name="planner")
+        return {"chat_history": [planner_response]}
+
+
+    async def worker_node(self, state: JockeyState, worker: AgentExecutor, name):
+        try:
+            worker_instructions = await self.worker_instruction_generator.ainvoke(state)
+            worker_instructions = HumanMessage(content=worker_instructions.content, name=f"{state['next_worker']}_instructions")
+        except Exception as error:
+            return {"chat_history": HumanMessage(
+                content=f"Got the following error when generating {state['next_worker']} instructions: {error}", 
+                name="instruction_generation_error"
+            )}
+        
+        try:
+            worker_response = await worker.ainvoke({"chat_history": [worker_instructions]})
+        except Exception as error:
+            return {"chat_history": HumanMessage(
+                content=f"Got the following error from the {state['next_worker']} worker when executing the following instructions: {worker_instructions.content}",
+                name="worker_error"
+            )}
+        
+        worker_response = json.dumps(worker_response, indent=2)
+        return {"chat_history": [worker_instructions, HumanMessage(content=worker_response, name=name)]}
+    
+
+    async def reflect_node(self, state: JockeyState):
         reflect_prompt = ChatPromptTemplate.from_messages([
             ("system", self.base_prompt),
             MessagesPlaceholder(variable_name="chat_history"),
             ("system", "Given the above context provide your final response.")
-        ]).partial(workers=worker_names)
+        ])
         reflect_chain = reflect_prompt | self.supervisor_llm
-        worker_response = await reflect_chain.ainvoke(state)
-        return {"chat_history": [worker_response]}
+        reflect_chain = reflect_chain.with_config({"tags": ["reflect"]})
+        reflect_response = await reflect_chain.ainvoke(state)
+        return {"chat_history": [reflect_response]}
     
 
     def construct_graph(self):
@@ -111,20 +175,30 @@ class Jockey(StateGraph):
         ]
 
         self.add_node("supervisor", self.supervisor)
+        self.add_node("planner", self.planner_node)
         self.add_node("reflect", self.reflect_node)
 
         for worker, node in zip(self.workers, worker_nodes):
             self.add_node(worker.name, node)
             self.add_edge(worker.name, "supervisor")
 
+        self.add_edge("planner", "supervisor")
+
         node_map = {worker.name: worker.name for worker in self.workers}
         node_map["REFLECT"] = "reflect"
+        node_map["planner"] = "planner"
         
         self.add_edge("reflect", END)
         self.add_conditional_edges(
             "supervisor",
-            lambda x: x["next"],
+            lambda x: x["next_worker"],
             node_map,
+        )
+
+        self.add_conditional_edges(
+            "planner",
+            lambda _: "supervisor",
+            {"supervisor": "supervisor"}
         )
 
         self.set_entry_point("supervisor")
