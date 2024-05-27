e54199676dfe311b1dcbecf74d66f6a7389309b1
Travis Couture <travis@twelvelabs.io>
2024-05-27 14:28:50 -0600

    Modularized simple wrapper for building jockey and created a dedicated example file for running a Jockey instance in a local terminal.

---
+++ jockey/jockey_default_server.py
@@ -0,0 +1,77 @@
+import os
+from typing import Union
+from langchain_openai import AzureChatOpenAI
+from langchain_openai import ChatOpenAI
+from jockey_graph import build_jockey_graph
+from jockey_graph import Jockey
+
+
+def build_jockey(
+        planner_llm: Union[AzureChatOpenAI | ChatOpenAI], 
+        supervisor_llm: Union[AzureChatOpenAI | ChatOpenAI], 
+        worker_llm: Union[AzureChatOpenAI | ChatOpenAI]) -> Jockey:
+    """Convenience function for standing up a local Jockey instance for dev work. 
+
+    Args:
+        planner_llm (Union[BaseChatOpenAI  |  AzureChatOpenAI]): 
+            The LLM used for the planner node. It is recommended this be a GPT-4 class LLM.
+
+        supervisor_llm (Union[BaseChatOpenAI  |  AzureChatOpenAI]): 
+            The LLM used for the supervisor. It is recommended this be a GPT-4 class LLM or better.
+
+        worker_llm (Union[BaseChatOpenAI  |  AzureChatOpenAI]): 
+            The LLM used for the planner node. It is recommended this be a GPT-3.5 class LLM or better.
+
+    Returns:
+        Jockey: A local Jockey instance.
+    """
+    
+    # Here we load all the required prompts for a Jockey instance.
+    supervisor_filepath = os.path.join(os.path.dirname(__file__), "prompts", "supervisor.md")
+    planner_filepath = os.path.join(os.path.dirname(__file__), "prompts", "planner.md")
+
+    with open(supervisor_filepath, "r") as supervisor_prompt_file:
+        supervisor_prompt = supervisor_prompt_file.read()
+
+    with open(planner_filepath, "r") as planner_prompt_file:
+        planner_prompt = planner_prompt_file.read()
+
+    return build_jockey_graph(
+        planner_llm=planner_llm,
+        planner_prompt=planner_prompt, 
+        supervisor_llm=supervisor_llm, 
+        supervisor_prompt=supervisor_prompt,
+        worker_llm=worker_llm
+    )
+
+# Here we construct all the LLMs for a Jockey instance.
+# Currently we only support OpenAI LLMs
+# Also note the class of LLM used for each component.
+# When implementing your own server you can import build_jockey separately or modify this file directly.
+# This allows you to choose your own LLMs.
+planner_llm = supervisor_llm = AzureChatOpenAI(
+    deployment_name="gpt-4",
+    streaming=True,
+    temperature=0,
+    model_version="1106-preview",
+    tags=["planner"]
+)
+
+supervisor_llm = AzureChatOpenAI(
+    deployment_name="gpt-4",
+    streaming=True,
+    temperature=0,
+    model_version="1106-preview",
+    tags=["supervisor"]
+)
+
+worker_llm = AzureChatOpenAI(
+    deployment_name="gpt-35-turbo-16k",
+    streaming=True,
+    temperature=0,
+    model_version="0613",
+    tags=["worker"]
+)
+
+# This variable is what is used by the LangGraph API server.
+jockey = build_jockey(planner_llm=planner_llm, supervisor_llm=supervisor_llm, worker_llm=worker_llm)
---
+++ jockey/jockey_dev_example.py
@@ -0,0 +1,40 @@
+import uuid
+import asyncio
+from dotenv import load_dotenv
+from rich.console import Console
+from util import parse_langchain_events_terminal
+from langchain_core.messages import HumanMessage
+from jockey_default_server import build_jockey, planner_llm, supervisor_llm, worker_llm
+
+
+load_dotenv()
+
+async def run_jockey():
+    """Quickstart function to create a Jockey instance in the terminal for easy dev work."""
+    jockey = build_jockey(planner_llm=planner_llm, supervisor_llm=supervisor_llm, worker_llm=worker_llm)
+
+    console = Console()
+
+    session_id = uuid.uuid4()
+
+    while True:
+        console.print()
+        user_input = console.input("[green]👤 Chat: ")
+
+        # Collect user input as a HumanMessage
+        # Reset the state of Jockey instance every new user invocation.
+        user_input = [HumanMessage(content=user_input, name="user")]
+        jockey_input = {
+            "chat_history": user_input,
+            "made_plan": False,
+            "next_worker": None,
+            "active_plan": None
+        }
+        async for event in jockey.astream_events(jockey_input, {"configurable": {"thread_id": session_id}}, version="v1"):
+            parse_langchain_events_terminal(event)
+
+        console.print()
+
+
+if __name__ == "__main__":
+    asyncio.run(run_jockey())
