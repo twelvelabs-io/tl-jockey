e7aff8558a8d6e6b9ce7bebc24f4fb52a4d0e532
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2024-06-03 16:34:46 -0700

    add_links_data

---
+++ src/components/Modal/helpers/links.js
@@ -0,0 +1,18 @@
+export const linksForYoutube = {
+    "Las Vegas Raiders vs. Kansas City Chiefs Game Highlights | NFL 2023 Week 16" : "https://www.youtube.com/watch?v=6dhCZ1xJT6U",
+    "Los Angeles Chargers vs. Las Vegas Raiders | 2023 Week 15 Game Highlights" : "https://www.youtube.com/watch?v=wwS12cMIhwg",
+    "New York Giants vs. Las Vegas Raiders | 2023 Week 9 Game Highlights" : "https://www.youtube.com/watch?v=MYnCCYCI_1Q",
+    "Las Vegas Raiders vs. Buffalo Bills Game Highlights | NFL 2023 Week 2" : "https://www.youtube.com/watch?v=UdDHY5AUAKg",
+    "Las Vegas Raiders vs. Los Angeles Chargers | 2023 Week 4 Game Highlights" : "https://www.youtube.com/watch?v=_K0ZDOoKtT0",
+    "New York Jets vs. Las Vegas Raiders | 2023 Week 10 Game Highlights" : "https://www.youtube.com/watch?v=WiMcrrxlSIg",
+    "Las Vegas Raiders vs. Indianapolis Colts | 2023 Week 17 Game Highlights" : "https://www.youtube.com/watch?v=bZIBit6x0H0",
+    "Green Bay Packers vs. Las Vegas Raiders | 2023 Week 5 Game Highlights" : "https://www.youtube.com/watch?v=-xTQmdnQbbg",
+    "Minnesota Vikings vs. Las Vegas Raiders | 2023 Week 14 Game Highlights" : "https://www.youtube.com/watch?v=U52oIDuo3AI",
+    "Las Vegas Raiders vs. Denver Broncos Game Highlights | NFL 2023 Week 1" : "https://www.youtube.com/watch?v=JEVmIDXz8GM",
+    "New England Patriots vs. Las Vegas Raiders | 2023 Week 6 Game Highlights" : "https://www.youtube.com/results?search_query=New+England+Patriots+vs.+Las+Vegas+Raiders+%7C+2023+Week+6+Game+Highlights",
+    "Las Vegas Raiders vs. Chicago Bears | 2023 Week 7 Game Highlights" : "https://www.youtube.com/watch?v=yeLl3rNThZE",
+    "Denver Broncos vs. Las Vegas Raiders Game Highlights | NFL 2023 Week 18" : "https://www.youtube.com/watch?v=fM4Br7tTE50",
+    "Kansas City Chiefs vs. Las Vegas Raiders Game Highlights | NFL 2023 Week 12" : "https://www.youtube.com/watch?v=lGOCc-r4tb8",
+    "Las Vegas Raiders vs. Detroit Lions | 2023 Week 8 Game Highlights" : "https://www.youtube.com/watch?v=w9egNPaosJY",
+    "Pittsburgh Steelers vs. Las Vegas Raiders | 2023 Week 3 Game Highlights" : "https://www.youtube.com/watch?v=ce-Jbe-p0ow"
+}
