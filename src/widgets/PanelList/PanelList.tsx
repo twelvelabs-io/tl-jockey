216b87318574d9b003952b3cc89c73bc27a01ac1
dmitrii <dmitrii@dmitriis-MacBook-Pro.local>
2024-06-03 18:48:04 -0700

    final_fixes_flexible_heigh

--- src/widgets/PanelList/PanelList.tsx
+++ src/widgets/PanelList/PanelList.tsx
@@ -12,7 +12,7 @@ interface PanelListProps {
 const PanelList: React.FC<PanelListProps> = ({ videos, refetchVideos }) => {
     //overflow-y-auto h-[100vh] pl-5 pr-5 flex flex-col gap-4
     return (
-        <div className="overflow-y-auto pl-5 pr-5 flex flex-col gap-4" style={{ height: "calc(100vh - 96px)" }}>
+        <div className="overflow-y-auto pl-5 pr-5 flex flex-col gap-4" style={{ height: "calc(100vh - 166px)" }}>
             {videos.map((video: any, index: any) => (
                 <ErrorBoundary FallbackComponent={ErrorFallBack} onReset={refetchVideos}>
                     <Suspense fallback={<FallBackVideoPlaceholder size='small'/>}>
