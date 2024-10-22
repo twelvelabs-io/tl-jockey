f56867dae9827198391660ffc7e4630f1a2cfe57
king12 <kingsotn-twelve@king12.local>
2024-10-21 22:40:15 -0700

    Revert changes made in jockey/__main__.py from commit 411c1f0

--- jockey/__main__.py
+++ jockey/__main__.py
@@ -5,16 +5,16 @@ from jockey.util import preflight_checks
 
 
 def main():
-    preflight_checks() # might move this to app.py near check_environment_variables().
+    # preflight_checks()
     mode = sys.argv[1] if len(sys.argv) > 1 else "terminal"
     if mode == "server":
-        return run_jockey_server()
-    while True:
-        try:
-            asyncio.run(run_jockey_terminal())
-        except KeyboardInterrupt:
-            print("\nExecution interrupted")
-            break
+        run_jockey_server()
+    else:
+        while True:
+            try:
+                asyncio.run(run_jockey_terminal())
+            except KeyboardInterrupt:
+                print("\nExecution interrupted")
 
 
 if __name__ == "__main__":
